8fa38de7967e8eb239a766fc60e9de44d3135922
marcogael1 <vacunas@uthh.edu.mx>
2024-05-16 20:55:52 -0600

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,11 @@ import { VaccineMonthModule } from './vaccine-month/vaccine-month.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 //mongodb://localhost:27017/vaccinemanager
-
+//mongodb+srv://vacunas:BlmpV4WEQaw1gUxI@clustervaccination.nny0wi6.mongodb.net/vaccinemanager
 @Module({
         imports: [
                 MongooseModule.forRoot(
-                        'mongodb+srv://vacunas:BlmpV4WEQaw1gUxI@clustervaccination.nny0wi6.mongodb.net/vaccinemanager',
+                        'mongodb+srv://vacunas:W0j3k2szYeMztEFM@clustervaccination.nny0wi6.mongodb.net/',
                 ),
                 VaccinesModule,
                 UsersModule,
