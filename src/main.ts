8eff2eb066c6984cdf778f13f26095ecd15644a4
marcogael1 <vacunas@uthh.edu.mx>
2024-05-30 14:06:58 -0600

    servidor

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
         const app = await NestFactory.create(AppModule);
-        app.useGlobalPipes(new ValidationPipe());
-        app.enableCors({
-                origin: ['http://localhost:5173', 'http://localhost:3000', 'https://example.com'],
-                methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-                allowedHeaders: 'Content-Type, Accept',
-        });
+        app.enableCors();
         await app.listen(3000);
 }
 bootstrap();
