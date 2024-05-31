6de744b9848a7bd4e30768c161d78d7567cabef4
marcogael1 <vacunas@uthh.edu.mx>
2024-05-31 10:03:35 -0600

    login web

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsMiddleware } from './middlewares/cors.middleware'; 
 
 async function bootstrap() {
         const app = await NestFactory.create(AppModule);
         app.useGlobalPipes(new ValidationPipe());
-        app.enableCors({
-                origin: '*',
-                methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-                allowedHeaders: 'Content-Type, Accept',
-                credentials: true, 
-        });
+        app.use(CorsMiddleware);
+
         await app.listen(3000);
 }
 bootstrap();
---
+++ src/middlewares/cors.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }
+}
