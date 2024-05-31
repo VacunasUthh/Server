ea26da4d0e5c9a2f2b7c3e2da1d68b4a1a132435
marcogael1 <vacunas@uthh.edu.mx>
2024-05-31 10:42:03 -0600

    web

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-        const app = await NestFactory.create(AppModule);
-        app.useGlobalPipes(new ValidationPipe());
-        app.enableCors({
-                origin: '*',
-                methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-                allowedHeaders: 'Content-Type, Accept',
-                credentials: true, 
-        });
-        await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  await app.listen(3000);
 }
 bootstrap();
