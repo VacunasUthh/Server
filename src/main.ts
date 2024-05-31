8afe73ed1521d75531f25b5510746f8140b8eabc
marcogael1 <vacunas@uthh.edu.mx>
2024-05-31 10:05:30 -0600

    login web

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { CorsMiddleware } from './middlewares/cors.middleware'; 
+import { CorsMiddleware } from './middlewares/cors.middleware'; // Importa el middleware
 
 async function bootstrap() {
         const app = await NestFactory.create(AppModule);
         app.useGlobalPipes(new ValidationPipe());
-        app.use(CorsMiddleware);
-
+        app.enableCors({
+                origin: '*',
+                methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+                allowedHeaders: 'Content-Type, Accept',
+                credentials: true, 
+        });
         await app.listen(3000);
 }
 bootstrap();
