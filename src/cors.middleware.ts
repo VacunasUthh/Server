5e1b7e834049b0d9b83c7fece81e120800a0aefe
marcogael1 <vacunas@uthh.edu.mx>
2024-06-01 19:39:59 -0600

    web

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,15 +4,18 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', '*');
+    // Establece los encabezados CORS
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Cambia esto según tu configuración
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
 
+    // Responde con un estado 204 No Content a las solicitudes OPTIONS
     if (req.method === 'OPTIONS') {
-      res.sendStatus(204);
-    } else {
-      next();
+      return res.sendStatus(204);
     }
+
+    // Pasa la solicitud al siguiente middleware o controlador
+    next();
   }
 }
